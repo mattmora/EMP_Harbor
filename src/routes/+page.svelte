452250860c0fe09b3dba68a1c184bd1fd5d3f18a
Matt <mw4854@nyu.edu>
2022-11-23 23:16:29 -0500

    better splitscreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,53 +3,75 @@
 	// import * as JoyCon from 'ns-joycon';
 
 	const maps = [];
-	const mapContexts = [];
-	const players = [];
-	const playerContexts = [];
+	const views = [];
 
-	const viewports = [];
-
+	let hiResCanvas;
+	let hiResContext;
 	let backgroundCanvas;
-	let animationCanvas;
 	let backgroundContext;
+	let animationCanvas;
 	let animationContext;
 
-	let innerWidth = 0;
-	let innerHeight = 0;
-
-	let x = 0;
-	let y = 0;
-	let xFollow = x;
-	let yFollow = y;
-
 	const input = {};
-	let zoom = 2;
 	const zoomRate = 0.2;
-	const moveSpeed = 1000;
+	const moveSpeed = 50;
 
 	const update = (delta) => {
-		const forward = input.e ?? 0;
-		const back = input.q ?? 0;
-		const z = forward - back;
-		const zd = Math.pow(1 + z * zoomRate, delta);
-		zoom *= zd;
-		const zx = (-x + innerWidth * 0.5) * zd * (zd - 1);
-		const zy = (-y + innerHeight * 0.5) * zd * (zd - 1);
-		const left = input.a ?? 0;
-		const right = input.d ?? 0;
-		const down = input.s ?? 0;
-		const up = input.w ?? 0;
-		const h = right - left;
-		const v = up - down;
-		const mag = Math.sqrt(h * h + v * v);
-		const n = mag > 0 ? 1 / mag : 0;
-		x -= n * h * moveSpeed * (1 / zoom) * delta + zx;
-		y += n * v * moveSpeed * (1 / zoom) * delta - zy;
+		views.forEach((player) => {
+			let { x, y, zoom } = player;
+
+			const forward = input.e ?? 0;
+			const back = input.q ?? 0;
+			const z = forward - back;
+			const zn = Math.pow(1 + z * zoomRate, delta);
+			zoom *= zn;
+
+			const left = input.a ?? 0;
+			const right = input.d ?? 0;
+			const down = input.s ?? 0;
+			const up = input.w ?? 0;
+			const h = right - left;
+			const v = up - down;
+			const mag = Math.sqrt(h * h + v * v);
+			const n = mag > 0 ? 1 / mag : 0;
+			x += n * h * moveSpeed * (1 / zoom) * delta;
+			y -= n * v * moveSpeed * (1 / zoom) * delta;
+
+			player.x = x;
+			player.y = y;
+			player.zoom = zoom;
+		});
 	};
 
 	const render = () => {
-		mapContexts.forEach((c) => c.drawImage(backgroundCanvas, 0, 0));
-		playerContexts.forEach((c) => c.drawImage(animationCanvas, 0, 0));
+		const hiResRatio = hiResCanvas.width / backgroundCanvas.width;
+
+		views.forEach(({ x, y, zoom, canvas, context, map }) => {
+			const width = (map.canvas.width = canvas.width = canvas.offsetWidth);
+			const height = (map.canvas.height = canvas.height = canvas.offsetHeight);
+
+			context.drawImage(animationCanvas, x, y, width / zoom, height / zoom, 0, 0, width, height);
+
+			const useHiRes = zoom > 0.8;
+			const scalar = useHiRes ? hiResRatio : 1;
+			const sourceCanvas = useHiRes ? hiResCanvas : backgroundCanvas;
+			const sx = x * scalar;
+			const sy = y * scalar;
+			const sw = (width * scalar) / zoom;
+			const sh = (height * scalar) / zoom;
+
+			map.context.drawImage(
+				sourceCanvas,
+				sx - sw * 0.5,
+				sy - sh * 0.5,
+				sw,
+				sh,
+				0,
+				0,
+				width,
+				height
+			);
+		});
 	};
 
 	let start, previous;
@@ -73,16 +95,14 @@
 	};
 
 	const hueRotate = 180;
-	const invert = 180;
+	const invert = 100;
+	const saturation = 1;
+
+	$: mapStyle = `
+    filter: hue-rotate(${hueRotate}deg) invert(${invert}%) saturate(${saturation});`;
 
 	const viewCount = 4;
 	const div = Math.ceil(Math.sqrt(viewCount));
-
-	$: mapStyle = `
-	top: ${y}px;
-	left: ${x}px;
-	transform: scale(${zoom});
-    filter: hue-rotate(${hueRotate}deg) invert(${invert}%);`;
 
 	const cellSize = 100 / div;
 	const sizeString = `${cellSize}% `;
@@ -93,18 +113,34 @@
 
 	onMount(() => {
 		// Offscreen canvases
+		hiResCanvas = document.createElement('canvas');
+		hiResContext = hiResCanvas.getContext('2d');
+
 		backgroundCanvas = document.createElement('canvas');
-		animationCanvas = document.createElement('canvas');
 		backgroundContext = backgroundCanvas.getContext('2d');
-		animationContext = backgroundCanvas.getContext('2d');
+
+		animationCanvas = document.createElement('canvas');
+		animationContext = animationCanvas.getContext('2d');
 
 		// Rendering canvases
 		for (let i = 0; i < viewCount; i++) {
-			viewports.push(document.getElementById(`viewport${i}`));
-			maps.push(document.getElementById(`map${i}`));
-			mapContexts.push(maps[i].getContext('2d'));
-			players.push(document.getElementById(`player${i}`));
-			playerContexts.push(players[i].getContext('2d'));
+			maps.push({});
+			maps[i].canvas = document.getElementById(`map${i}`);
+			maps[i].context = maps[i].canvas.getContext('2d');
+
+			views.push({
+				x: 0,
+				y: 0,
+				zoom: 1,
+				map: maps[i]
+			});
+			views[i].canvas = document.getElementById(`player${i}`);
+			views[i].context = views[i].canvas.getContext('2d');
+
+			views.forEach(({ canvas, map }) => {
+				map.canvas.width = canvas.width = canvas.offsetWidth;
+				map.canvas.height = canvas.height = canvas.offsetHeight;
+			});
 		}
 
 		// On image load, set canvas sizes and render the background
@@ -117,18 +153,20 @@
 			animationCanvas.width = image.naturalWidth;
 			animationCanvas.height = image.naturalHeight;
 
-			maps.forEach((c) => {
-				c.width = image.naturalWidth;
-				c.height = image.naturalHeight;
-			});
-
-			players.forEach((c) => {
-				c.width = image.naturalWidth;
-				c.height = image.naturalHeight;
-			});
-
 			backgroundContext.drawImage(image, 0, 0);
-			mapContexts.forEach((c) => c.drawImage(backgroundCanvas, 0, 0));
+
+			maps.forEach(({ canvas: c, context: ctx }) => {
+				ctx.drawImage(backgroundCanvas, 0, 0, c.width, c.height, 0, 0, c.width, c.height);
+			});
+		};
+
+		const hiRes = new Image();
+		hiRes.src = 'chart-10000.png';
+		hiRes.onload = () => {
+			hiResCanvas.width = hiRes.naturalWidth;
+			hiResCanvas.height = hiRes.naturalHeight;
+
+			hiResContext.drawImage(hiRes, 0, 0);
 		};
 
 		requestAnimationFrame(step);
@@ -147,18 +185,13 @@
 	});
 </script>
 
-<svelte:window
-	bind:innerWidth
-	bind:innerHeight
-	on:keydown|preventDefault={onKeyDown}
-	on:keyup|preventDefault={onKeyUp}
-/>
+<svelte:window on:keydown|preventDefault={onKeyDown} on:keyup|preventDefault={onKeyUp} />
 
 <div class="views" style={gridStyle}>
 	{#each [...Array(viewCount).keys()] as _, i}
 		<div id="viewport{i}" class="viewport">
 			<canvas id="map{i}" class="map" style={mapStyle} />
-			<canvas id="player{i}" class="player" style={mapStyle} />
+			<canvas id="player{i}" class="player" />
 		</div>
 	{/each}
 </div>
@@ -179,13 +212,17 @@
 	}
 
 	.map {
-		position: relative;
+		width: 100%;
+		height: 100%;
+		position: absolute;
 		transform-origin: top left;
 		z-index: 1;
 	}
 
 	.player {
-		position: relative;
+		width: 100%;
+		height: 100%;
+		position: absolute;
 		transform-origin: top left;
 		z-index: 2;
 	}
