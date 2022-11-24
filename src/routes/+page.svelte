23f0098b55921690e0eb6dc2a94256b4ba12cf24
Matt <mw4854@nyu.edu>
2022-11-23 21:05:49 -0500

    working split screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,30 @@
 	import { onMount } from 'svelte';
 	// import * as JoyCon from 'ns-joycon';
 
+	const maps = [];
+	const mapContexts = [];
+	const players = [];
+	const playerContexts = [];
+
+	const viewports = [];
+
+	let backgroundCanvas;
+	let animationCanvas;
+	let backgroundContext;
+	let animationContext;
+
 	let innerWidth = 0;
 	let innerHeight = 0;
 
 	let x = 0;
 	let y = 0;
+	let xFollow = x;
+	let yFollow = y;
 
 	const input = {};
-	let zoom = 1000;
+	let zoom = 2;
 	const zoomRate = 0.2;
-	const moveSpeed = 500;
+	const moveSpeed = 1000;
 
 	const update = (delta) => {
 		const forward = input.e ?? 0;
@@ -29,19 +43,94 @@
 		const v = up - down;
 		const mag = Math.sqrt(h * h + v * v);
 		const n = mag > 0 ? 1 / mag : 0;
-		x -= n * h * moveSpeed * (1000 / zoom) * delta + zx;
-		y += n * v * moveSpeed * (1000 / zoom) * delta - zy;
+		x -= n * h * moveSpeed * (1 / zoom) * delta + zx;
+		y += n * v * moveSpeed * (1 / zoom) * delta - zy;
+	};
+
+	const render = () => {
+		mapContexts.forEach((c) => c.drawImage(backgroundCanvas, 0, 0));
+		playerContexts.forEach((c) => c.drawImage(animationCanvas, 0, 0));
 	};
 
 	let start, previous;
 	const step = (timestamp) => {
 		if (!start) start = timestamp;
 		const delta = timestamp - previous;
-		if (delta > 0) update(delta / 1000);
+		if (delta > 0) {
+			update(delta / 1000);
+			render();
+		}
 		previous = timestamp;
 		requestAnimationFrame(step);
 	};
+
+	const onKeyDown = (e) => {
+		input[e.key.toLowerCase()] = 1;
+	};
+
+	const onKeyUp = (e) => {
+		input[e.key.toLowerCase()] = 0;
+	};
+
+	const hueRotate = 180;
+	const invert = 180;
+
+	const viewCount = 4;
+	const div = Math.ceil(Math.sqrt(viewCount));
+
+	$: mapStyle = `
+	top: ${y}px;
+	left: ${x}px;
+	transform: scale(${zoom});
+    filter: hue-rotate(${hueRotate}deg) invert(${invert}%);`;
+
+	const cellSize = 100 / div;
+	const sizeString = `${cellSize}% `;
+
+	$: gridStyle = `
+	grid-template-columns: ${sizeString.repeat(div)};
+	grid-template-rows: ${sizeString.repeat(div)};`;
+
 	onMount(() => {
+		// Offscreen canvases
+		backgroundCanvas = document.createElement('canvas');
+		animationCanvas = document.createElement('canvas');
+		backgroundContext = backgroundCanvas.getContext('2d');
+		animationContext = backgroundCanvas.getContext('2d');
+
+		// Rendering canvases
+		for (let i = 0; i < viewCount; i++) {
+			viewports.push(document.getElementById(`viewport${i}`));
+			maps.push(document.getElementById(`map${i}`));
+			mapContexts.push(maps[i].getContext('2d'));
+			players.push(document.getElementById(`player${i}`));
+			playerContexts.push(players[i].getContext('2d'));
+		}
+
+		// On image load, set canvas sizes and render the background
+		const image = new Image();
+		image.src = 'chart-5000.png';
+		image.onload = () => {
+			backgroundCanvas.width = image.naturalWidth;
+			backgroundCanvas.height = image.naturalHeight;
+
+			animationCanvas.width = image.naturalWidth;
+			animationCanvas.height = image.naturalHeight;
+
+			maps.forEach((c) => {
+				c.width = image.naturalWidth;
+				c.height = image.naturalHeight;
+			});
+
+			players.forEach((c) => {
+				c.width = image.naturalWidth;
+				c.height = image.naturalHeight;
+			});
+
+			backgroundContext.drawImage(image, 0, 0);
+			mapContexts.forEach((c) => c.drawImage(backgroundCanvas, 0, 0));
+		};
+
 		requestAnimationFrame(step);
 
 		// JoyCon.findControllers((devices) => {
@@ -54,24 +143,8 @@
 		// 		});
 		// 		await device.enableIMU();
 		// 	});
-		// });
+		// });a
 	});
-
-	const onKeyDown = (e) => {
-		input[e.key.toLowerCase()] = 1;
-	};
-
-	const onKeyUp = (e) => {
-		input[e.key.toLowerCase()] = 0;
-	};
-
-    const hueRotate = 0;
-    const invert = 0;
-
-	$: mapStyle = `width: ${zoom}%;
-    height: ${zoom}%;
-    background-position: ${x}px ${y}px; 
-    filter: hue-rotate(${hueRotate}deg) invert(${invert}%);`;
 </script>
 
 <svelte:window
@@ -81,32 +154,39 @@
 	on:keyup|preventDefault={onKeyUp}
 />
 
-<div class="viewport">
-	<div class="map" style={mapStyle} />
-	<div class="ship">0</div>
+<div class="views" style={gridStyle}>
+	{#each [...Array(viewCount).keys()] as _, i}
+		<div id="viewport{i}" class="viewport">
+			<canvas id="map{i}" class="map" style={mapStyle} />
+			<canvas id="player{i}" class="player" style={mapStyle} />
+		</div>
+	{/each}
 </div>
 
 <style>
-	.viewport {
+	.views {
 		position: absolute;
+		width: 100%;
+		height: 100%;
+		display: grid;
+	}
+
+	.viewport {
+		position: relative;
 		width: 100%;
 		height: 100%;
 		overflow: hidden;
 	}
 
 	.map {
-		background: white url(./chart.png) no-repeat;
-		background-size: cover;
+		position: relative;
+		transform-origin: top left;
+		z-index: 1;
 	}
 
-	.ship {
-		color: magenta;
-		position: absolute;
-		width: fit-content;
-		margin: 0;
-		top: 50%;
-		left: 50%;
-		-ms-transform: translate(-50%, -50%);
-		transform: translate(-50%, -50%);
+	.player {
+		position: relative;
+		transform-origin: top left;
+		z-index: 2;
 	}
 </style>
